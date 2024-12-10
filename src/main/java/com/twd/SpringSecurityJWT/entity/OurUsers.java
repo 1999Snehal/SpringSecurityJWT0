6a14a78ad19c762d61d890766733925657854def
Admin <snehallagad84@gmail.com>
2024-12-10 15:32:20 +0530

    commit

---
+++ src/main/java/com/twd/SpringSecurityJWT/entity/OurUsers.java
@@ -0,0 +1,76 @@
+
+package com.twd.SpringSecurityJWT.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "ourusers")
+public class OurUsers implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String email;
+    private String password;
+    private String role;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setPassword(String encode) {
+    }
+
+
+
+    public void setEmail(String email) {
+    }
+
+    public void setRole(Object role) {
+
+    }
+
+    public int getId() {
+        return  0;
+    }
+}
